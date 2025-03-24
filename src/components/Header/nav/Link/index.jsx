4567a08460b1e61441eff0bd4cd529e9e00cd2c1
Brian <briankgathui@gmail.com>
2025-03-24 17:19:17 +0300

    Key changes for deployment

--- src/components/Header/nav/Link/index.jsx
+++ src/components/Header/nav/Link/index.jsx
@@ -1,27 +1,32 @@
 import styles from './style.module.scss';
 import { motion } from 'framer-motion';
 import { slide, scale } from '../../animation';
+import Link from 'next/link';
 
-export default function NavLink({data, isActive, setSelectedIndicator}) {
-  
-    const { title, href, index} = data;
-  
-    return (
-      <motion.div 
-        className={styles.link} 
-        onMouseEnter={() => {setSelectedIndicator(href)}} 
-        custom={index} 
-        variants={slide} 
-        initial="initial" 
-        animate="enter" 
-        exit="exit"
-      >
-        <motion.div 
-          variants={scale} 
-          animate={isActive ? "open" : "closed"} 
-          className={styles.indicator}>
-        </motion.div>
-        <Link href={href}>{title}</Link>
-      </motion.div>
-    )
+export default function NavLink({ data, isActive, setSelectedIndicator }) {
+  const { title, href, index } = data;
+
+  return (
+    <motion.div
+      className={styles.link}
+      onMouseEnter={() => {
+        setSelectedIndicator(href);
+      }}
+      custom={index}
+      variants={slide}
+      initial="initial"
+      animate="enter"
+      exit="exit"
+    >
+      <motion.div
+        variants={scale}
+        animate={isActive ? 'open' : 'closed'}
+        className={styles.indicator}
+      ></motion.div>
+
+      <Link href={href} legacyBehavior>
+        <a>{title}</a>
+      </Link>
+    </motion.div>
+  );
 }
